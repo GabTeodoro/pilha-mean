d4c583e01b1a37f993e5dc2fd240434fa053d1f6
Gabriel Teodoro <teodoro.gt@hotmail.com>
2021-05-03 23:25:03 -0300

    Refactored routes and add upload files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MatInputModule } from '@angular/material/input';
@@ -29,7 +30,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    ReactiveFormsModule,
+    //FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
